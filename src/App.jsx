dda2d4a45e827b2ae4d48158d086ea51156e85e0
Audrey Medaino-Tardif <audreymedaino-tardif@Audreys-MacBook-Air.local>
2024-11-18 23:12:04 -0500

    Getting annoyed af

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Search } from "./Search.jsx";
 import './App.css';
 
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
 
 export default function App() {
 
--- src/Hit.jsx
+++ src/Hit.jsx
@@ -1,5 +1,5 @@
 import { Highlight } from "react-instantsearch";
-import { getPropertyByPath } from 'instantsearch.js/es/lib/utils';
+//import { getPropertyByPath } from 'instantsearch.js/es/lib/utils';
 
 export const Hit = ({ hit }) => {
   return (
