c99a1a87c5a9b16030b46115e287c1d0a28db3be
Audrey Medaino-Tardif <audreymedaino-tardif@Audreys-MacBook-Air.local>
2024-11-18 22:48:51 -0500

    Rebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './src/App.jsx';
+import App from '/Users/audreymedaino-tardif/contechhq/fran/src/App.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
